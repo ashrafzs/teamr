103b89aa44d4245a89116f62bf7b2808377030d9
Ashraf Zohair Al-Saqqa <ashraf.eb@hotmail.com>
2018-06-03 11:46:17 +0300

    clean up project

--- Teamr.Core/DataAccess/CoreDbContext.cs
+++ Teamr.Core/DataAccess/CoreDbContext.cs
@@ -10,7 +10,6 @@ namespace Teamr.Core.DataAccess
 		{
 		}
 
-		public virtual DbSet<SomeThing> SomeThings { get; set; }
 		public virtual DbSet<ActivityType> ActivityTypes { get; set; }
 		public virtual DbSet<Activity> Activities { get; set; }
 		public virtual DbSet<RegisteredUser> Users { get; set; }
@@ -18,7 +17,6 @@ namespace Teamr.Core.DataAccess
 		protected override void OnModelCreating(ModelBuilder builder)
 		{
 			base.OnModelCreating(builder);
-			builder.ApplyConfiguration(new SomeThingMap());
 			builder.ApplyConfiguration(new ActivityTypeMap());
 			builder.ApplyConfiguration(new ActivityMap());
 			builder.ApplyConfiguration(new RegisteredUserMap());
