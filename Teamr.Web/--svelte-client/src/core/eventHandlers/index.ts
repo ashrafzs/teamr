e1f0905e4a263b1373a20d52567c1376a9126c75
nehad <nehad_136@hotmail.com>
2018-07-18 09:06:56 +0300

    remove folder

--- Teamr.Web/--svelte-client/src/core/eventHandlers/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./form/FormLogToConsole";
-export * from "./form/ReloadFormAfterAction";
-
-export * from "./input/BindToOutput";
-export * from "./input/InputLogToConsole";
-export * from "./input/DependOn";
-
-export * from "./output/OutputLogToConsole";
--- Teamr.Web/--svelte-client/src/core/framework/IFunctionRunner.ts
+++
@@ -1,5 +0,0 @@
-import { ClientFunctionMetadata } from "uimf-core";
-
-export interface IFunctionRunner {
-	run(params: ClientFunctionMetadata, args?: any): Promise<void>;
-}
